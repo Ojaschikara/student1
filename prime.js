9c4fe4a515951458728e9e4aa1fa8da14f3cb5b5
Ojaschikara <ojaschikara@email.com>
2024-01-09 20:55:56 +0530

    added

---
+++ prime.js
@@ -0,0 +1,15 @@
+function isPrime(num) {
+    let bag="";
+     for(let i = 2; i < num; i++) {
+       
+        if(num % i == 0) {
+            bag++;
+        }
+      
+       
+     } 
+    if(bag>0){return `${num} is not a prime number`}
+     else{return `${num} is a prime number`}
+    
+  }
+  console.log(isPrime(9));
